525e3dafa62f919bcfae1fae9273c67df8602be2
Ruth Westnidge <rwestnidge@gmail.com>
2022-03-08 12:18:22 +0000

    Adds style.css adds GUI

--- script.js
+++ script.js
@@ -1,87 +1,140 @@
+// declare global variables
 let wins = 0;
 let losses = 0;
+let roundsPlayed = 0;
+let resultsDivRef = document.getElementById('results');
 
-function computerPlay() {
+
+//returns random word, called in playRound();
+// highlights 'Binky's chosen button' 
+computerPlay = () => {
   const play = ["rock", "scissors", "paper"];
+  play.forEach(unGlow);
   let randomNumber = Math.floor(Math.random() * 3);
   const returnWord = play[randomNumber];
+  const binkyClass = '.binky' + returnWord;
+  const binkyChoice = document.querySelector(binkyClass);
+  binkyChoice.classList.add('glow');
   return returnWord;
 }
 
+// removes action of line 17 prevents highlight 'sticking'
+unGlow = (item) => {
+  let binkybtn = document.querySelector('.binky' + item);
+  binkybtn.classList.remove('glow');
+}
 
-function playRound() {
+// allows reuse existing code PlayRound() 
+//by hardcoding a string for each button 
+// and displays playerSelection 
+const playerrock = document.querySelector('#playerrock');
+playerrock.addEventListener('click', () => {
+  let playerSelection = playRound('rock');
+  let finalResult = finishGame();
+  resultsDivRef.textContent = playerSelection + ' ' + finalResult;
+  return playerSelection;
+});
+
+const playerpaper = document.querySelector('#playerpaper');
+playerpaper.addEventListener('click', () => {
+  let playerSelection = playRound('paper');
+  let finalResult = finishGame();
+  resultsDivRef.textContent = playerSelection + ' ' + finalResult;
+  return playerSelection;
+});
+
+const playerscissors = document.querySelector('#playerscissors');
+playerscissors.addEventListener('click', () => {
+  let playerSelection = playRound('scissors');
+  let finalResult = finishGame();
+  resultsDivRef.textContent = playerSelection + ' ' + finalResult;
+
+  return playerSelection;
+});
+
+const restartButton = document.querySelector('.restart button');
+restartButton.addEventListener('click', () => {
+  location.reload();
+});
+
+const finishGame = () => {
+  if (roundsPlayed == 5) {
+    let theButtons = document.querySelector('.buttons');
+    theButtons.style.display = "none";
+    let binkyButtons = document.querySelector('.binkybtn');
+    binkyButtons.style.display = 'none';
+    let theRestart = document.querySelector('.restart');
+    theRestart.style.display = 'block';
+    if (wins == losses) {
+      console.log("Game Over! That's a tie!");
+      return ("Game Over! That's a tie!");
+    } else if (wins > losses) {
+      console.log("Game over! You win!");
+      return ("Game over! You win!");
+    } else {
+      console.log("Game over! You lose!");
+      return ("Game over! You lose!");
+    }
+  } else {
+    return ' ';
+  }
+}
+
+function playRound(playerSelection) {
   let computerSelection = computerPlay();
   console.log(computerSelection);
-  let playerSelection = prompt("Choose how you will wipe the floor with your oppponent! Enter rock, scissors, or paper.", "word");
-  playerSelection = playerSelection.toLowerCase();
   console.log(playerSelection);
+
 
   if (computerSelection == playerSelection) {
     console.log("That's a tie!");
-    return "That's a tie!";
+    roundsPlayed++;
+    return "That's a tie! Meh.";
   }
 
   if (computerSelection == "rock") {
     if (playerSelection == "paper") {
       console.log("You win! Paper beats rock!");
       wins++;
-      return "You win! Paper beats rock!";
+      roundsPlayed++;
+      return "You win! Your paper beats Binky's rock!";
     }
 
     if (playerSelection == "scissors") {
       console.log("You lose! Rock beats scissors!");
       losses++;
-      return "You lose! Rock beats scissors!";
+      roundsPlayed++;
+      return "You lose! Binky's rock beat your scissors!";
     }
 
   } else if (computerSelection == "scissors") {
     if (playerSelection == "paper") {
       console.log("You lose! Scissors beat paper!");
       losses++;
-      return "You lose! Scissors beat paper!";
+      roundsPlayed++;
+      return "You lose! Binky's scissors beat your paper!";
     }
 
     if (playerSelection == "rock") {
       console.log("You win! Rock beats scissors!");
       wins++;
-      return "You win! Rock beats scissors!";
+      roundsPlayed++;
+      return "You win! Your rock beats Binky's scissors!";
     }
 
   } else { //computerSelection === "paper" 
     if (playerSelection == "rock") {
-      console.log("You lose! Paper beats rock!");
+      console.log("You lose! Rock beats paper!");
       losses++;
-      return "You lose! Paper beats rock!";
+      roundsPlayed++;
+      return "You lose! Binky's paper beats your rock!";
     }
 
     if (playerSelection == "scissors") {
       console.log("You win! Scissors beat paper!");
       wins++;
-      return "You win! Scissors beat paper!";
+      roundsPlayed++;
+      return "You win! Your scissors beat Binky's paper!";
     }
   }
-  console.log("Oops! You didn't type in \"rock\", \"scissors\" or \"paper\". Please check your spelling and try again.");
-  return false;
 }
-
-
-/*function game() {
-  wins = 0;
-  losses = 0;
-  for (let i = 0; i < 5; i++) {
-    playRound();
-  }
-
-  if (wins == losses) {
-    console.log("Game Over! That's a tie!");
-  } else if (wins > losses) {
-    console.log("Game over! You win!");
-  } else {
-    console.log("Game over! You lose!");
-  }
-}
-
-game();
-/*
-
-
